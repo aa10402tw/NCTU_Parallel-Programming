5f201d3925175205880fe2d66a4ea3426ff5161b
CKW_TW <aa10402tw@gmail.com>
2019-11-04 14:50:18 +0800

    update

--- OpenMP/test_omp.c
+++ OpenMP/test_omp.c
@@ -8,10 +8,10 @@ int main(int argc, char* argv[])
     int vec[10000];
     for(int i=0; i<10000; i++)
         vec[i] = i;
-    printf("Sum %d", compute_mat_sum(mat));
+    printf("Sum %d", compute_mat_sum(vec));
 } 
 
-int compute_mat_sum(int mat[]){
+int compute_mat_sum(int vec[]){
     int sum = 0;
     for(int i=0; i<10000; i++)
         sum += vec[i];
