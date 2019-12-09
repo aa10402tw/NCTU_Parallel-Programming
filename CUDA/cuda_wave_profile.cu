0d84e241c300f840ade1314939fdfee16c4cc94b
CKW_TW <aa10402tw@gmail.com>
2019-12-09 12:38:18 +0800

    update:

---
+++ CUDA/cuda_wave_profile.cu
@@ -0,0 +1,193 @@
+/**********************************************************************
+ * DESCRIPTION:
+ *   Serial Concurrent Wave Equation - C Version
+ *   This program implements the concurrent wave equation
+ *********************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define MAXPOINTS 1000000
+#define MAXSTEPS 1000000
+#define MINPOINTS 20
+#define PI 3.14159265
+
+void check_param(void);
+void printfinal(void);
+
+/**********************************************************************
+ *  Checks input values from parameters
+ *********************************************************************/
+void check_param(int nsteps, int tpoints)
+{
+    char tchar[20];
+
+    /* check number of points, number of iterations */
+    while ((tpoints < MINPOINTS) || (tpoints > MAXPOINTS)) {
+        printf("Enter number of points along vibrating string [%d-%d]: "
+            , MINPOINTS, MAXPOINTS);
+        scanf("%s", tchar);
+        tpoints = atoi(tchar);
+        if ((tpoints < MINPOINTS) || (tpoints > MAXPOINTS))
+            printf("Invalid. Please enter value between %d and %d\n",
+                MINPOINTS, MAXPOINTS);
+    }
+    while ((nsteps < 1) || (nsteps > MAXSTEPS)) {
+        printf("Enter number of time steps [1-%d]: ", MAXSTEPS);
+        scanf("%s", tchar);
+        nsteps = atoi(tchar);
+        if ((nsteps < 1) || (nsteps > MAXSTEPS))
+            printf("Invalid. Please enter value between 1 and %d\n", MAXSTEPS);
+    }
+    printf("Using points = %d, steps = %d\n", tpoints, nsteps);
+}
+
+/**********************************************************************
+ *     Print final results
+ *********************************************************************/
+void printfinal(float* values, int tpoints)
+{
+    int i;
+
+    for (i = 1; i <= tpoints; i++) {
+        printf("%6.4f ", values[i]);
+        if (i % 10 == 0)
+            printf("\n");
+    }
+}
+
+__global__ void init_line_kernel(float* oldval, float* values, int tpoints) {
+    //int i, j;
+    float x, fac, k, tmp;
+    
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    int offset = blockDim.x * gridDim.x; // Total number of threads
+    fac = 2.0 * PI;
+    tmp = (float)(tpoints - 1);
+
+    for (int idx = tid; idx <= tpoints; idx += offset) {
+        if (idx >= 1) {
+            k = (float)(idx - 1);
+            x = k / tmp;
+            values[idx] = __sinf(fac * x);
+            oldval[idx] = values[idx];
+        }
+    }
+}
+
+__global__ void update_kernel(float* oldval, float* values, float* newval, int nsteps, int tpoints)
+{
+    int i, j, tid, offset;
+    tid = blockIdx.x * blockDim.x + threadIdx.x; // threadId
+    offset = blockDim.x * gridDim.x; // Total number of threads
+
+    /* Update values for each time step */
+    for (i = 1; i <= nsteps; i++) {
+        for (j = tid; j <= tpoints; j += offset) {
+            /* global endpoints */
+            if ((j == 1) || (j == tpoints)) {
+                newval[j] = 0.0;
+            }
+            else {
+                float dtime, c, dx, tau, sqtau;
+                dtime = 0.3;
+                c = 1.0;
+                dx = 1.0;
+                tau = (c * dtime / dx);
+                sqtau = tau * tau;
+                newval[j] = (2.0 * values[j]) - oldval[j] + (sqtau *  (-2.0)*values[j]);
+            }
+            /* Update old values with new values */
+            oldval[j] = values[j];
+            values[j] = newval[j];
+        }
+    }
+}
+
+
+/**********************************************************************
+ *  Main program
+ *********************************************************************/
+int main(int argc, char *argv[])
+{
+    int nsteps,                     /* number of time steps */
+        tpoints,                    /* total points along string */
+        rcode;                      /* generic return code */
+	
+	clock_t begin, end;
+	double time_elapsed;
+
+    /* Read Parameters */
+	begin = clock();
+    sscanf(argv[1], "%d", &tpoints);
+    sscanf(argv[2], "%d", &nsteps);
+    check_param(nsteps, tpoints);
+	end = clock();
+	time_elapsed = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Time to Read Parameters %f\n", time_elapsed);
+
+    /* Setup Kernel Launch Parameters */
+    int threadsPerBlock = 512;
+    int numBlocks = (tpoints / threadsPerBlock) + 1;
+
+	
+    /* Allocate Host Memory */
+	begin = clock();
+    float *oldval, *values, *newval;
+    oldval = (float*)malloc((tpoints + 2) * sizeof(float)); /* values at time (t-dt) */
+    values = (float*)malloc((tpoints + 2) * sizeof(float)); /* values at time t */
+    newval = (float*)malloc((tpoints + 2) * sizeof(float)); /* values at time (t+dt) */
+	end = clock();
+	time_elapsed = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Time to Allocate Host Memory %f\n", time_elapsed);
+
+    /* Allocate Device Memory */
+	begin = clock();
+    float *gpu_oldval, *gpu_values, *gpu_newval;
+    cudaMalloc(&gpu_oldval, (tpoints + 2) * sizeof(float));
+    cudaMalloc(&gpu_values, (tpoints + 2) * sizeof(float));
+    cudaMalloc(&gpu_newval, (tpoints + 2) * sizeof(float));
+	cudaDeviceSynchronize();
+	end = clock();
+	time_elapsed = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Time to Allocate Device Memory %f\n", time_elapsed);
+
+    /* Initialize Wave */
+	begin = clock();
+    //printf("Initializing points on the line...\n");
+    init_line_kernel <<<numBlocks, threadsPerBlock>>> (gpu_oldval, gpu_values, tpoints);
+	cudaDeviceSynchronize();
+	end = clock();
+	time_elapsed = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Time to Initialize Wave %f\n", time_elapsed);
+
+    /* Update Wave */
+	begin = clock();
+    //printf("Updating all points for all time steps...\n");
+    update_kernel <<<numBlocks, threadsPerBlock>>> (gpu_oldval, gpu_values, gpu_newval, nsteps, tpoints);
+	cudaDeviceSynchronize();
+	end = clock();
+	time_elapsed = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Time to Update Wave %f\n", time_elapsed);
+
+    /* Copy Memory from Device to Host */
+	begin = clock();
+    cudaMemcpy(oldval, gpu_oldval, (tpoints + 2) * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(values, gpu_values, (tpoints + 2) * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(newval, gpu_newval, (tpoints + 2) * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaDeviceSynchronize();
+	end = clock();
+	time_elapsed = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Time to Copy Memory from Device to Host %f\n", time_elapsed);
+
+    /* Print Result */
+	begin = clock();
+    //printf("Printing final results...\n");
+    //printfinal(values, tpoints);
+    //printf("\nDone.\n\n");
+	end = clock();
+	time_elapsed = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("Time to Print Result %f\n", time_elapsed);
+    return 0;
+}
